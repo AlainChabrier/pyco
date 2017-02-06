81325ca72f9194bfecf889b7e2b6158ef7114810
Alain Chabrier <achabrier@es.ibm.com>
2017-02-06 15:22:42 +0000

    package

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name="co",
     version="1.0.0",
     long_description=__doc__,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+    packages=['co',],
 )
