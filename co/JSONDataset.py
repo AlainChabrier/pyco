9869c44fc101d3b3c5c4fce342d2647e7d82e4c1
Alain Chabrier <achabrier@es.ibm.com>
2017-02-07 14:52:29 +0100

    json data set

--- co/JSONDataset.py
+++ co/JSONDataset.py
@@ -1,11 +1,22 @@
 import json
 
 class JSONDataset:
+    def __init__(self):
+        self.data = {}
+        self.data['name'] = 'unnamed'
+        self.data['tables'] = {}
+
     def __init__(self, data):
         self.data = data
 
     def __str__(self):
         return "JSON dataset " + str(self.data["name"])
+
+    def setName(self, name):
+        self.data['name'] = name
+
+    def addTable(self, tableName, rows):
+        self.data['tables'][tableName]['rows'] = rows
 
     def to_json(self):
         return json.dumps(self.data)
